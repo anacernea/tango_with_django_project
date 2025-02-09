5c8836c576eed1f4c3c1ed566060d1daa6ad00df
“Ana <3078518C@student.gla.ac.uk>
2025-02-09 15:29:14 +0000

    Chapter 4: Templates and Media Files

--- rango/views.py
+++ rango/views.py
@@ -14,6 +14,11 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    #return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    #context_dict = {'boldmessage': 'This tutorial  has been put together by Ana Cernea.'}
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'rango/about.html', context = {})
 
 
